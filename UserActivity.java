0425691cff042e6c074ecbdac7211aa5730c8677
harris2232 <harrisahmed599@gmail.com>
2019-05-26 23:23:38 +0500

    Create UserActivity.java
    New user activity that will show user all the stories and other data. Just create and kept basic code, shall be updated later.

---
+++ UserActivity.java
@@ -0,0 +1,17 @@
+package com.sqe.project.ladderup;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class UserActivity extends AppCompatActivity {
+
+
+
+    @Override
+    public void onCreate(Bundle saved){
+        super.onCreate(saved);
+        setContentView(R.layout.activity_user);
+    }
+
+
+}
