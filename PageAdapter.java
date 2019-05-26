b99ad828b70303ac852697e95727c70f9f1eec45
harris2232 <harrisahmed599@gmail.com>
2019-05-26 23:21:37 +0500

    Update PageAdapter.java
    Added Overridden functions.

--- PageAdapter.java
+++ PageAdapter.java
@@ -1,6 +1,35 @@
-package com.sqe.project.tablayoutexample;
+package com.sqe.project.ladderup;
 
-public class PageAdapter{
-private int tabCount;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 
+public class PageAdapter extends FragmentPagerAdapter {
+
+    private int numOfTabs;
+
+    public PageAdapter(FragmentManager fm, int numOfTabs) {
+        super(fm);
+        this.numOfTabs = numOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        switch (i){
+            case 0:
+                //return new StoriesTab();
+            case 1:
+                //return new YourFeedTab();
+            case 2:
+                //return new ProfileTab();
+            default:
+                return null;
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return numOfTabs;
+    }
 }
