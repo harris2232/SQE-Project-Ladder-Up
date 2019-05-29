980d6954312a86188a671ec790fa2fc519fd5127
harris2232 <harrisahmed599@gmail.com>
2019-05-29 15:59:03 +0500

    Update ProfileTab.java
    Added Profile Image, Name and Email load feature.

--- ProfileTab.java
+++ ProfileTab.java
@@ -1,15 +1,107 @@
 package com.sqe.project.ladderup;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
 
 public class ProfileTab extends Fragment {
 
+    ImageView profilePicture;
+    TextView name, email;
+    FirebaseAuth mAuth;
+    FirebaseUser user;
+    DatabaseReference databaseReference;
+    FirebaseStorage firebaseStorage;
+    StorageReference storageReference;
+    FirebaseUser firebaseUser;
+    ProgressBar imageLoad;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab_profile, container, false);
+        View view = inflater.inflate(R.layout.tab_profile, container, false);
+        profilePicture = view.findViewById(R.id.profilepicture_view);
+        name = view.findViewById(R.id.profile_name);
+        email = view.findViewById(R.id.profile_email);
+        imageLoad = view.findViewById(R.id.image_load);
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+        firebaseUser = mAuth.getCurrentUser();
+        databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(user.getUid());
+        firebaseStorage = FirebaseStorage.getInstance();
+        storageReference = firebaseStorage.getReferenceFromUrl("gs://ladderupsqe.appspot.com/ProfilePictures").child(firebaseUser.getUid()+".jpg");
+
+        final File localFile;
+        try {
+            localFile = File.createTempFile("images", "jpg");
+            storageReference.getFile(localFile)
+                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                            Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+                            profilePicture.setImageBitmap(bitmap);
+                            imageLoad.setVisibility(View.GONE);
+                            Toast.makeText(getActivity(), "Image Loaded", Toast.LENGTH_LONG).show();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    Toast.makeText(getActivity(), exception.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                name.setText(dataSnapshot.child("name").getValue().toString());
+                email.setText(dataSnapshot.child("email").getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                Snackbar.make(getView(), databaseError.getMessage(), Snackbar.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+
+
+
+
+
+        return view;
     }
 }
